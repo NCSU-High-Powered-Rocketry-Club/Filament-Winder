0d8d286e0d9c2f729ec82ddab3be01767b655413
arkey <arkey@ncsu.edu>
2026-09-07 21:06:14 -0400

    Corrected DEFAULT_AXIS_STEPS_PER_UNIT & moved DEFAULT_MAX_FEEDRATE back to original defaults

--- Marlin-bugfix-2.1.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.1.x/Marlin/Configuration.h
@@ -1360,7 +1360,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 1/(0.01*M_PI*0.891*25.4), 80/9, 160/9, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200*16/(2*M_PI*0.891*25.4), 80/9, 160/9, 500 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
